d1d4322349853f65983f2f9b437ae64600ed088c
relzinga7 <93522617+relzinga7@users.noreply.github.com>
2022-01-03 14:20:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let yourAge = 18;
+
+if (yourAge < 17) {
+
+    console.log("You are not allowed in to the pub. You are kindly requested to stay outside");
+}
+
+else {
+
+    console.log("You are allowed in to the pub"); 
+}
+
+let isFemale = true;
+
+if (isFemale) {
+
+    console.log("You are allowed in to the pub");
+}
+
+else {
+
+    console.log("You are not allowed in to the pub. You are kindly requested to stay outside"); 
+}
+
+let driverStatus = "designated";
+
+if (driverStatus == "designated") {
+
+    console.log("You are allowed to drive");
+}
+
+else {
+
+    console.log("You are not allowed to drive");
+}
